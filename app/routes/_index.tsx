39041590a32a607cb4b22cc6fad36e53ef08f3f9
nurdin najib <najibnurdin999@gmail.com>
2024-12-06 13:45:44 +0700

    added dependencies

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,11 +11,6 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <Link
-        to={`/about`}
-      >
-        About
-      </Link>
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
           <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
